67130e94064ba8a4be40461b9199739dbad8634a
Philip Samoei <samphilkerich04@gmail.com>
2023-03-15 08:38:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+console.log(multiply)
 
-// Write your code here
+const random = Math.floor(Math.random()*10) + 1
+console.log(random)
+
+let num3 = 12
+let num4 = 3
+const mod = num3 / num4
+console.log(mod)
+
+const max = 20
+console.log(Math.max(max))
